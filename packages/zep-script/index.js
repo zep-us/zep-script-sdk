4da818d2d205eef88ea71cc45adb50990468843f
ZEP <109573682+zepdevelopers@users.noreply.github.com>
2023-03-31 18:34:31 +0900

    Update index.js

--- packages/zep-script/index.js
+++ packages/zep-script/index.js
@@ -1,13 +1,1 @@
 import { ScriptPlayer } from "./src/ScriptPlayer";
-import {PutObjectType} from "./src/ScriptMap";
-
-export {};
-
-ScriptMap.putObjectMultiple([[0,1]],PutObjectType.stroke)
-
-ScriptApp.onJoinPlayer.Add(function (player){
-    player.showBuyAlert("1",3,()=>{
-        
-    },false,{message: "dd",}
-)
-})
