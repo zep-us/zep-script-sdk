429fdf588d885353e9729f601c9f741e3a573848
김우재 <woojae@naverz-corp.com>
2023-06-09 13:50:06 +0900

    fix: get vite root from config

--- packages/vite-plugin-zep-script/src/index.ts
+++ packages/vite-plugin-zep-script/src/index.ts
@@ -1,18 +1,20 @@
-import react from '@vitejs/plugin-react-swc'
-import type {Plugin,UserConfig} from 'vite'
+import react from "@vitejs/plugin-react-swc";
+import type { Plugin, UserConfig } from "vite";
 import path from "path";
 import fs from "fs-extra";
 
 const zepScriptPlugin = (): Plugin => ({
-  name: 'mutate-config',
+  name: "mutate-config",
   config(config) {
-    const configJsonPath = path.join(process.cwd(), "zep-script.json");
+    const root = config.root ?? process.cwd();
+    const configJsonPath = path.join(root, "zep-script.json");
+    const widgetPath = path.join(root, "widget");
     const zepScriptConfig = JSON.parse(
       fs.readFileSync(configJsonPath).toString()
     );
     const pluginConfig: UserConfig = {
       plugins: [...(config.plugins || []), react()],
-      root: 'widget',
+      root: widgetPath,
       publicDir: false,
       build: {
         ...config.build,
@@ -21,14 +23,14 @@ const zepScriptPlugin = (): Plugin => ({
       experimental: {
         ...config.experimental,
         renderBuiltUrl(filename) {
-          if (filename.startsWith('/')) {
+          if (filename.startsWith("/")) {
             filename = filename.substring(1);
           }
           filename = `/spaces/app/file?appHashId=${zepScriptConfig.appId}&fileName=widget/${filename}`;
           return filename;
-        }
-      }
-    }
+        },
+      },
+    };
     Object.assign(config, pluginConfig);
   },
 });
