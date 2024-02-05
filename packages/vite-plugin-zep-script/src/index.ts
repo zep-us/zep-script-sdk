3e98a845a878c582eefc9f3ef213934b3211b271
김우재 <woojae@naverz-corp.com>
2024-02-05 19:13:51 +0900

    chore: change internal appId resolution logic

--- packages/vite-plugin-zep-script/src/index.ts
+++ packages/vite-plugin-zep-script/src/index.ts
@@ -3,18 +3,46 @@ import type { Plugin, UserConfig } from "vite";
 import path from "path";
 import fs from "fs-extra";
 
+function getAppId(config: any) {
+  if (typeof config.appId === "string") {
+    return config.appId;
+  } else if (typeof config.appId === "object") {
+    const baseUrl = process.env.BASE_URL;
+    let environment = "dev";
+    if (baseUrl) {
+      try {
+        const url = new URL(baseUrl);
+        if (url.hostname === "zep.us") {
+          environment = "live";
+        } else if (url.hostname.endsWith(".zep.us")) {
+          environment = url.hostname.replace(".zep.us", "");
+        } else {
+          console.error("Invalid BASE_URL");
+        }
+      } catch (error) {
+        console.error("Invalid BASE_URL");
+      }
+    }
+    const appIdForEnvironment = config.appId[environment];
+    if (typeof appIdForEnvironment !== "string") {
+      throw new Error(`appId for environment ${environment} not found`);
+    }
+    return appIdForEnvironment;
+  }
+}
+
 const zepScriptPlugin = (): Plugin => ({
   name: "zep-script",
   config(config) {
     const root = config.root ?? process.cwd();
     const widgetPath = path.join(root, "widget");
-    const configJsonPath =
-      process.env.ZEP_SCRIPT_CONFIG_PATH || path.join(root, "zep-script.json");
+    const configJsonPath = path.join(root, "zep-script.json");
     const zepScriptConfig = JSON.parse(
       fs.readFileSync(configJsonPath).toString()
     );
+    const appId = getAppId(zepScriptConfig);
     const pluginConfig: UserConfig = {
-      base: `/app/${zepScriptConfig.appId}/widget`,
+      base: `/app/${appId}/widget`,
       plugins: [...(config.plugins || []), react()],
       root: widgetPath,
       publicDir: false,
